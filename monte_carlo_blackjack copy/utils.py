953b6d1697df21933c3cc335424715fc79967c10
Rikdojo <79783040+Rikdojo@users.noreply.github.com>
2025-07-07 10:05:23 +0200

    Add files via upload

---
+++ monte_carlo_blackjack copy/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def argmax_action(Q, s):
+    actions = [a for (state, a) in Q if state == s]
+    if not actions:
+        return None
+    values = [Q[(s, a)] for a in actions]
+    max_index = np.argmax(values)
+    return actions[max_index]
